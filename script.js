e2cf5f39f6f6ebb51553ccfb46f606f58362e0ac
jayaxsurya <jayasurya.dev@outlook.com>
2024-04-01 18:07:27 +0530

    final files are added

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+let button = document.getElementById("read");
+
+button.addEventListener('click', () => {
+
+    
+    document.querySelectorAll('.single-box').forEach(e =>{
+        e.classList.remove('unseen')
+    });
+
+    document.querySelectorAll('.dot').forEach(e =>{
+        e.classList.remove('dot')
+    });
+
+    document.getElementById('number').innerHTML = 0;
+})
+
